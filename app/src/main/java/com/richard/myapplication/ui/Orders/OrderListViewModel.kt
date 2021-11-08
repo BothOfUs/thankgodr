f62fe3f84c600fcc44c22231ee0520f57171dd6c
ThankGod Richard <madueketf@gmail.com>
2021-11-09 00:12:44 +0100

    Added view model and mimic data source from websrvice or API

--- app/src/main/java/com/richard/myapplication/ui/Orders/OrderListViewModel.kt
+++ app/src/main/java/com/richard/myapplication/ui/Orders/OrderListViewModel.kt
@@ -3,11 +3,24 @@ package com.richard.myapplication.ui.Orders
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.richard.myapplication.models.Orders
+import com.richard.myapplication.ui.Orders.repository.OrderDataSource
 
 class OrderListViewModel : ViewModel() {
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is dashboard Fragment"
+   private lateinit var odersLiveData : MutableLiveData<ArrayList<Orders>>
+   private  lateinit var oderRepo : OrderDataSource
+
+
+    fun init(){
+         if(odersLiveData != null){
+             return
+         }
+        oderRepo = OrderDataSource
+        odersLiveData = oderRepo.getOrdersfromSource()
     }
-    val text: LiveData<String> = _text
+
+    fun getOrders() : LiveData<ArrayList<Orders>>{
+        return odersLiveData
+    }
 }
--- app/src/main/java/com/richard/myapplication/ui/Orders/OrdersListFragment.kt
+++ app/src/main/java/com/richard/myapplication/ui/Orders/OrdersListFragment.kt
@@ -8,11 +8,17 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.richard.myapplication.R
+import com.richard.myapplication.models.Orders
+import com.richard.myapplication.ui.Orders.adapters.OrderListAdapter
 
 class OrdersListFragment : Fragment() {
 
     private lateinit var orderListViewModel: OrderListViewModel
+    lateinit var recyclerView :RecyclerView
+    lateinit var adapter: OrderListAdapter;
 
     override fun onCreateView(
             inflater: LayoutInflater,
@@ -21,11 +27,29 @@ class OrdersListFragment : Fragment() {
     ): View? {
         orderListViewModel =
                 ViewModelProvider(this).get(OrderListViewModel::class.java)
+        orderListViewModel.init()
+
+
         val root = inflater.inflate(R.layout.list_order_fragment, container, false)
-        val textView: TextView = root.findViewById(R.id.listOrderRecycler)
-        orderListViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
+        initRecyclerView(root)
+
+        orderListViewModel.getOrders().observe(viewLifecycleOwner, {
+            adapter.notifyDataSetChanged()
         })
+
+        initRecyclerView(root)
         return root
+    }
+
+    fun initRecyclerView(root : View){
+        recyclerView = root.findViewById<RecyclerView>(R.id.listOrderRecycler)
+        adapter = context?.let { orderListViewModel.getOrders().value?.let { it1 ->
+            OrderListAdapter(
+                it1, it)
+        } }!!
+        val layoutManger = LinearLayoutManager(context, LinearLayoutManager.VERTICAL,false)
+        recyclerView.layoutManager = layoutManger
+        recyclerView.adapter = adapter
+
     }
 }
